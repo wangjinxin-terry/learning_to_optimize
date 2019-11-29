e667d2ec3eaecb232d969a8b430c8c47bba80c87
terrywang <Jinxin_Wang@pku.edu.cn>
2019-11-29 19:34:23 +0800

    amend readme.md

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 ## LISTA
+This repository is using Pytorch for reproducing the LISTA, TiLISTA and LISTA_CP.
+
+###Data generation
+see prob.py. We generate 
+the matrix A: M(250) * N(500) We sample the
+entries of A i.i.d. from the standard Gaussian distribution, Aij ∼ N (0, 1/M) and then normalize its
+columns to have the unit l_2 norm. 
+
+measure y: y = A * x
+
+basis x: We decide each of its entry to be non-zero following the Bernoulli distribution with p_b = 0.1.
+The values of the non-zero entries are sampled from the standard Gaussian distribution
+
+We generate 10000 data for training and 1000 for testing.
+
+### Model
+16 layers of neural networks, W and S(not shared for each layer) as well as theta are learned.
+
+### Training
+loss: the output of each layer x_h. \Sum_h (x_h - x)^2
+optimizer: Adam
+lr: 5e-4
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.optim.lr_scheduler import StepLR
 from lista_dataset import LISTADataset
 from lista_model import LISTA
 
@@ -68,19 +67,16 @@ def main():
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=3, metavar='N',
+    parser.add_argument('--epochs', type=int, default=20, metavar='N',
                         help='number of epochs to train (default: 20)')
-    parser.add_argument('--lr', type=float, default=1e-4, metavar='LR',
+    parser.add_argument('--lr', type=float, default=5e-4, metavar='LR',
                         help='learning rate (default: 5e-4)')
-    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
-                        help='Learning rate step gamma (default: 0.7)')
     parser.add_argument('--no-cuda', action='store_true', default=False,
                         help='disables CUDA training')
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
     parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                         help='how many batches to wait before logging training status')
-
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
     args = parser.parse_args()
@@ -110,11 +106,9 @@ def main():
 
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
-    scheduler = StepLR(optimizer, step_size=4, gamma=args.gamma)
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch)
         test(args, model, device, test_loader)
-        scheduler.step()
 
     if args.save_model:
         torch.save(model.state_dict(), "lista.pt")
